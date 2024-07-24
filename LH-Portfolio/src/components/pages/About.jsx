90df427926758ede94125f976981352883e4ba01
Lachlan <lachyhughes@bigpond.com>
2024-07-24 11:25:35 +0930

    Added base navigations for different tabs. Have started to add some information. Will not worry about styling and structure until later after most of the content has been added.

---
+++ LH-Portfolio/src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        My name is Lachlan Hughes, I am 26 years old, born and raised in South Australia. I have recently begun my journey to programming
+        and I am excited to continue to grow. My personal hobbies include gaming, either solo or with friends. Gaming has also somewhat inspired me
+        to persure programming, as sometimes coding can feel like a puzzle game. You know how the end result will look, its just a matter of putting the
+        right pieces into place!
+      </p>
+    </div>
+  );
+}
